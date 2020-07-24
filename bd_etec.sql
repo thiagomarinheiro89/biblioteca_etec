9e03e58fcb87d274d95f3a10e31bd6c23bf60676
Thiago Marinheiro <marinheirocriacoes@gmail.com>
2020-07-23 21:52:23 -0300

    backup bd

--- bd_etec.sql
+++ bd_etec.sql
@@ -24,16 +24,36 @@ CREATE TABLE IF NOT EXISTS `tb_alunos` (
   `email` varchar(50) DEFAULT NULL,
   `telefone` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`matricula`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela bd_etec.tb_alunos: ~2 rows (aproximadamente)
+-- Copiando dados para a tabela bd_etec.tb_alunos: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `tb_alunos` DISABLE KEYS */;
 INSERT INTO `tb_alunos` (`matricula`, `nome`, `data_nasc`, `email`, `telefone`) VALUES
-	(1, 'Aluna Teste', '1989-01-03', 'teste@teste.com.br', '1140512755'),
-	(2, 'Aluna teste 2', '1982-03-30', 'teste2@teste.com.br', '123456'),
-	(3, 'Aluna Teste 3', '1989-01-30', 'email3@teste.com.br', '1156987453'),
-	(4, 'Thiago', '1989-01-03', 'marinheirocriacoes@gmail.com', '11 985674123');
+	(1, 'Aline Tatsuo Matanacombi', '1995-03-27', 'aline.matanacombi@etec.sp.gov.br', '(11) 4057-8965'),
+	(2, 'Bruna Santana', '1992-05-30', 'bruna.santana@etec.sp.gov.br', '(11) 96785-3146'),
+	(3, 'Fernada Pépreto', '1999-01-27', 'pepreto@gmail.com', '(11) 5728-2455'),
+	(4, 'Thiago Izidorio Marinheiro', '1989-01-03', 'marinheirocriacoes@gmail.com', '11 985674123'),
+	(5, 'Pedro antonio Javier', '1997-03-20', 'javier@gmail.com', '(11) 4044-3965');
 /*!40000 ALTER TABLE `tb_alunos` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela bd_etec.tb_emprestimo
+CREATE TABLE IF NOT EXISTS `tb_emprestimo` (
+  `id_emprestimo` int(11) NOT NULL AUTO_INCREMENT,
+  `id_obra` int(11) DEFAULT NULL,
+  `matricula` int(11) DEFAULT NULL,
+  `data_emprestimo` date DEFAULT NULL,
+  `data_devolucao` date DEFAULT NULL,
+  `status` varchar(1) DEFAULT NULL COMMENT 'E = Emprestado | F = Finalizado | A = Atrasado',
+  PRIMARY KEY (`id_emprestimo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- Copiando dados para a tabela bd_etec.tb_emprestimo: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `tb_emprestimo` DISABLE KEYS */;
+INSERT INTO `tb_emprestimo` (`id_emprestimo`, `id_obra`, `matricula`, `data_emprestimo`, `data_devolucao`, `status`) VALUES
+	(1, 3, 1, '2020-07-16', '2020-07-20', 'E'),
+	(2, 5, 1, '2020-07-20', '2020-07-20', 'F'),
+	(3, 5, 4, '2020-07-20', '2020-07-20', 'F');
+/*!40000 ALTER TABLE `tb_emprestimo` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela bd_etec.tb_modulos
 CREATE TABLE IF NOT EXISTS `tb_modulos` (
@@ -42,16 +62,17 @@ CREATE TABLE IF NOT EXISTS `tb_modulos` (
   `arquivo_base` varchar(255) DEFAULT NULL,
   `ordem` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_modulo`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 -- Copiando dados para a tabela bd_etec.tb_modulos: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `tb_modulos` DISABLE KEYS */;
 INSERT INTO `tb_modulos` (`id_modulo`, `titulo`, `arquivo_base`, `ordem`) VALUES
 	(1, 'Obras', 'livros.html', 1),
-	(2, 'Emprestimos', 'Emprestimos.php', 2),
+	(2, 'Emprestimos', 'movimentacao/menu.php', 2),
 	(3, 'Usuários', 'usuarios.php', 4),
 	(4, 'Alunos', 'alunos/alunos.php', 3),
-	(5, 'Relatórios', 'relatorios.php', 5);
+	(5, 'Relatórios', 'relatorios.php', 5),
+	(6, 'Dashboard', 'dash.php', 0);
 /*!40000 ALTER TABLE `tb_modulos` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela bd_etec.tb_obras
